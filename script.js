ed83962df8c48e935585d41a2c4eabc7e6113ad5
amiksha125 <amikshagajbhiye@gmail.com>
2025-05-24 18:56:33 +0530

    storing notes permanently on localstorage

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ const createNoteBtn = document.querySelector(".btn");
 
 let notes = document.querySelectorAll(".input-box");
 
+function showNotes(){
+    notesContainer.innerHTML = localStorage.getItem("notes"); // if the notes are stored in browser this func will display whenever u open.
+}
+
+showNotes();
+
 createNoteBtn.addEventListener("click", () => {
     let inputBox = document.createElement("p");
     let img = document.createElement("img");
@@ -21,5 +27,21 @@ createNoteBtn.addEventListener("click", () => {
 notesContainer.addEventListener("click", function(e){
     if(e.target.tagName === "IMG"){
         e.target.parentElement.remove();
+        updateStorage();
+    } else if(e.target.tagName === "P"){
+        notes = document.querySelectorAll(".input-box");
+        notes.forEach(note =>{
+            note.onkeyup = function(){
+                updateStorage();
+            }
+        })
     }
 })
+
+// to store your ntes after refresh 
+
+function updateStorage(){
+    localStorage.setItem("notes", notesContainer.innerHTML); // whatever there in a notes para box it is stored in our browser
+
+
+}
